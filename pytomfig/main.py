a6e4a5e029199e2d188eb6b27d754c937e8c2b98
Luke Shuttleworth <sluke950@gmail.com>
2024-08-06 08:18:45 -0700

    updated logging, began framework for more file types

--- pytomfig/main.py
+++ pytomfig/main.py
@@ -1,58 +1,66 @@
 import tomllib
+import json
+import os
+import logging
+
+LOGGER = logging.getLogger(__name__)
+
 
 class Config:
     _instance = None
 
-    def __new__(cls, file_path='config.toml'):
-        """Creates new instance, loads super init and the toml configuration file.
-
-        Args:
-            file_path (str, optional): File path to configuration toml file. Defaults to 'config.toml'.
-
-        Returns:
-            Config: instance.
-        """
+    def __new__(cls, config_file_path='config.toml', logging_config_file_path=None):
         if cls._instance is None:
             cls._instance = super(Config, cls).__new__(cls)
-            cls._instance._load_config(file_path)
+            cls._instance._setup(config_file_path, logging_config_file_path)
         return cls._instance
 
-    def _load_config(self):
-        """
-        Loads configuration data from the specified TOML file into the instance.
+    def _setup(self, config_file_path, logging_config_file_path):
+        self.config = self._load_config(config_file_path)
+        self.logging_config = self._load_config(
+            logging_config_file_path) if logging_config_file_path else None
 
-        This method reads the configuration file specified by `self.config_file_path` and 
-        parses its content using the `tomllib` module. The parsed configuration data is 
-        stored in the instance attribute `_config`.
+    def _load_config(self, file_path):
+        try:
+            file_extension = os.path.splitext(file_path)[1]
 
-        Raises:
-            FileNotFoundError: If the file specified by `self.config_file_path` does not exist.
-            IsADirectoryError: If the path specified by `self.config_file_path` is a directory.
-            tomllib.TOMLDecodeError: If there is an error parsing the TOML file.
+            LOGGER.debug(f'Loading {file_extension} file {file_path}.')
+            if file_extension == '.toml':  # TOML files
+                with open(file_path, 'rb') as file:
+                    return tomllib.load(file)
+            elif file_extension == '.json':  # JSON files
+                return json.load(file_path)
+            elif file_extension == '.yaml' or file_extension == '.yml':  # YAML files
+                return
+            elif file_extension == '.ini':  # INI files
+                return
+            elif file_extension == '.xml':  # XML files
+                return
+            elif file_extension == '.properties':  # Properties files
+                return
+            elif file_extension == '.env':  # .env files
+                return
+            elif file_extension == '.sh' or file_extension == '.bash':  # Shell files
+                return
+            elif file_extension == '.md':  # Markdown files
+                return
+            elif file_extension == '.config':  # Config files
+                return
+            else:
+                raise ValueError(
+                    f"Invalid file extension: {file_path}. Expected a '.toml', '.json', or '.yaml' file.")
 
-        Returns:
-            None
-        """
-        with open(self.config_file_path, 'r') as file:
-            self._config = tomllib.load(file)
+        except Exception as e:
+            LOGGER.exception(f'Could not load configuration.')
+            raise
 
     def get(self, key, default=None):
-        """Return a value from the config toml file.
-
-        Args:
-            key (str): Parameter name.
-            default (Any, optional): Default value to return if value can't be found in the config file. Defaults to None.
-
-        Returns:
-            Any: the value from the configuration file.
-        """
         keys = key.split('.')
         value = self.config
         for key in keys:
             value = value.get(key, default)
             if value is None:
-                #log warning no value found, using default
+                LOGGER.warn(f'No value found for [{key}], using default.')
                 return default
+        LOGGER.debug(f'Value [{value}] found for key [{key}].')
         return value
-                
-            
